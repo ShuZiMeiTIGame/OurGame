c11f424b5e3fd0bf3ea287de918363beada053b0
SpurYang <326019331@qq.com>
2015-04-13 14:50:45 +0800

    modify Tollgate---add ballPosiotn

--- Classes/Tollgate.cpp
+++ Classes/Tollgate.cpp
@@ -1,10 +1,11 @@
 #include"Tollgate.h"
-Tollgate::Tollgate(int level, bool isPassed){
+Tollgate::Tollgate(int level, bool isPassed,Vec2 pos){
 	_level = level;
 	_isPassed = isPassed;
+	_pos = pos;
 }
-Tollgate* Tollgate::createTollgate(int level, bool isPassed){
-	return new Tollgate(level, isPassed);
+Tollgate* Tollgate::createTollgate(int level, bool isPassed,Vec2 pos){
+	return new Tollgate(level, isPassed,pos);
 }
 int Tollgate::getLevel(){
 	return _level;
@@ -18,4 +19,9 @@ bool Tollgate::isPassed(){
 Vector<TollgateBody*>* Tollgate::getbodiesArray(){
 	return &_bodies;
 }
-
+void Tollgate::setBallPosition(Vec2 pos){
+	_pos = pos;
+}
+Vec2 Tollgate::getBallPostion(){
+	return _pos;
+}
--- Classes/Tollgate.h
+++ Classes/Tollgate.h
@@ -5,16 +5,18 @@ USING_NS_CC;
 class Tollgate
 {
 public:
-	Tollgate(int level, bool isPassed);
-	static Tollgate* createTollgate(int level, bool isPassed);
+	Tollgate(int level, bool isPassed,Vec2 pos);
+	static Tollgate* createTollgate(int level, bool isPassed, Vec2 pos);
 	int getLevel();
 	void setLevel(int level);
 	bool isPassed();
+	void setBallPosition(Vec2 pos);
+	Vec2 getBallPostion();
 	Vector<TollgateBody*>* getbodiesArray();
 private:
 	int _level;
 	bool _isPassed;
-
+	Vec2 _pos;
 	Vector<TollgateBody*> _bodies;
 };
 #endif
