d8329ee2ababb4fe1ba6923c5d181038fc85e7c3
githubwang2 <pokemonwang2@126.com>
2015-05-11 19:07:12 +0800

    draw Polygen

--- Classes/PhysicsEngine.cpp
+++ Classes/PhysicsEngine.cpp
@@ -1,6 +1,6 @@
 #include"PhysicsEngine.h"
 
-DrawNode*PhysicsWor::addBall(Vec2 p, int r, int c)
+DrawNode*PhysicsWor::addBall(Vec2 p, int r, float c)
 {
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 a1 = p - Vec2(r / 1.44, r / 1.44);
@@ -17,13 +17,29 @@ DrawNode*PhysicsWor::addBall(Vec2 p, int r, int c)
 	draw->drawDot(Vec2::ZERO, width, Color4F::WHITE);
 	draw->setPhysicsBody(ballBody);
 	return draw;
+	// DrawPrimitives::drawSolidCircle((a + b) / 2, (a - b).x, CC_DEGREES_TO_RADIANS(90), 50, 1.0f, 1.0f);
+	//return ball;
 }
+
+//Sprite*PhysicsWor::addBall(Vec2 a, int b)
+//{
+//	Size visibleSize = Director::getInstance()->getVisibleSize();
+//	auto ball = Sprite::create("CloseNormal.png");
+//	ball->setPosition(a);
+//	ball->setScale(2 * b / ball->getContentSize().width);
+//	PhysicsBody* ballBody = PhysicsBody::createCircle(b, PHYSICSBODY_MATERIAL_DEFAULT);
+//	ballBody->getShape(0)->setFriction(0.5f);
+//	ballBody->getShape(0)->setDensity(0.1f);
+//	ball->setPhysicsBody(ballBody);
+//	return ball;
+//}
 
 DrawNode* PhysicsWor::addBox(Vec2 a, Vec2 b, float c)
 {
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	int width = abs((a - b).x);
 	int height = abs((a - b).y);
+
 	Point boxPoint = Vec2(width, height);
 	PhysicsBody* boxBody = PhysicsBody::createBox(Size(boxPoint));
 	boxBody->getShape(0)->setRestitution(0.0f);
@@ -50,17 +66,60 @@ DrawNode *PhysicsWor::addSan(Vec2 a, std::vector<Vec2>* pos)
 		i++;
 	}
 	Vec2 c = c1 / j;
+	/////////////////////
+	//int arrL = sizeof(points) / sizeof(points[0]);
 	Point poi[3];
 	for (int i = 0; i < j; i++)
 	{
 		poi[i] = points[i] - c;
 		
 	}
+	///////
+	/*Vec2 a1 = points[0];
+	Vec2 a2 = points[1];
+	Vec2 a3 = points[2];*/
+
+	//float w = (a3 + a1).x;
+	//float h = (a2 + a1).y;
+	//Vec2 c = Vec2(w, h);
+	//Vec2 b1 = a1 - c;
+	//Vec2 b2 = a2 - c;
+	//Vec2 b3 = a3 - c;
+	//等腰
+	//float w = (a3 - a1).x;
+	//float h = (a2 - a1).y;
+	//Vec2 c = Vec2(w, h);
+	//Vec2 b1 = Vec2(-w / 2, -h / 2);
+	//Vec2 b2 = Vec2(0, h / 2);
+	//Vec2 b3 = Vec2(w / 2, -h / 2);
+	//Vec2 poi[3] = { b1, b2, b3 };
+	//san->setScale(2 * (a3 - a1).x / san->getContentSize().width);
+	//特列
+	//float w = (a3 + a1).x / 2;
+	//float h = (a2 + a1).y / 2;
+	//Vec2 b2 = Vec2(a2.x - w, a2.y - h);
+	//Vec2 b1 = Vec2(a1.x - w, a1.y - h);
+	//Vec2 b3 = Vec2(a3.x - w, a3.y - h);
+	//Vec2 poi[3] = { b1, b2, b3 };
+	////
+
+	/*Vec2 c = (a1 + a2 + a3) / 3;
+	Vec2 b1 = a1 - c;
+	Vec2 b2 = a2 - c;
+	Vec2 b3 = a3 - c;
+	Vec2 poi[3] = { b1, b2, b3 }; */
 	PhysicsBody*sanBody = PhysicsBody::createPolygon(poi, j, PHYSICSBODY_MATERIAL_DEFAULT, Vec2::ZERO);
 	sanBody->getShape(0)->setRestitution(0);
 	sanBody->getShape(0)->setFriction(1.0f);
 	sanBody->getShape(0)->setDensity(0.5f);
+	/*auto san = Sprite::create();
+	san->setPosition(a);
+	san->setPhysicsBody(sanBody);*/
+	//sanBody->setDynamic(false);
+	//sanBody->getShape(0)->recenterPoints(poi, 3, Vec2(0,0));
 	DrawNode* draw = DrawNode::create();
+	//draw->setAnchorPoint(Vec2::ZERO);
+	//draw->setAnchorPoint(a);
 	draw->setPosition(a);
 	draw->drawPolygon(poi, 3, Color4F::WHITE, 1, Color4F::WHITE);
 	draw->setPhysicsBody(sanBody);
@@ -73,29 +132,120 @@ DrawNode*PhysicsWor::addPolygon(Vec2 p, std::vector<Vec2>* pos)
 	Point points[20];
 	Vec2 c1 = Vec2(0, 0);
 	int j = 0;
-	auto i = pos->begin();
-	while (i != pos->end()){
+	auto i = pos->end()-1;
+	while (i != pos->begin()-1){
 		c1 += *i;
 		points[j++] = *i;
-		i++;
+		i--;
 	}
 	Vec2 c = c1 / j;
+	//int arrL = sizeof(points) / sizeof(points[0]);
 	Point poi[20];
 	for (int i = 0; i < j; i++)
 	{
 		poi[i] = points[i] - c;
-
 	}
 	PhysicsBody*sanBody = PhysicsBody::createPolygon(poi, j, PHYSICSBODY_MATERIAL_DEFAULT, Vec2::ZERO);
 	sanBody->getShape(0)->setRestitution(0);
 	sanBody->getShape(0)->setFriction(1.0f);
 	sanBody->getShape(0)->setDensity(0.5f);
+
 	DrawNode* draw = DrawNode::create();
 	draw->setPosition(p);
 	draw->drawPolygon(poi, 3, Color4F::WHITE, 1, Color4F::WHITE);
 	draw->setPhysicsBody(sanBody);
 	return draw;
 }
+
+//DrawNode *PhysicsWor::addSan(Vec2 a, std::vector<Vec2>* pos)
+//{
+//	Size visibleSize = Director::getInstance()->getVisibleSize();
+//
+//	Point points[3];
+//	int j = 0;
+//	auto i = pos->begin();
+//	while (i != pos->end()){
+//		points[j++] = *i;
+//		i++;
+//	}
+//	Vec2 a1 = points[0];
+//	Vec2 a2 = points[1];
+//	Vec2 a3 = points[2];
+//	//float w = (a3 + a1).x;
+//	//float h = (a2 + a1).y;
+//	//Vec2 c = Vec2(w, h);
+//	//Vec2 b1 = a1 - c;
+//	//Vec2 b2 = a2 - c;
+//	//Vec2 b3 = a3 - c;
+//	//等腰
+//	//float w = (a3 - a1).x;
+//	//float h = (a2 - a1).y;
+//	//Vec2 c = Vec2(w, h);
+//	//Vec2 b1 = Vec2(-w / 2, -h / 2);
+//	//Vec2 b2 = Vec2(0, h / 2);
+//	//Vec2 b3 = Vec2(w / 2, -h / 2);
+//	//Vec2 poi[3] = { b1, b2, b3 };
+//	//san->setScale(2 * (a3 - a1).x / san->getContentSize().width);
+//	//特列
+//	//float w = (a3 + a1).x / 2;
+//	//float h = (a2 + a1).y / 2;
+//	//Vec2 b2 = Vec2(a2.x - w, a2.y - h);
+//	//Vec2 b1 = Vec2(a1.x - w, a1.y - h);
+//	//Vec2 b3 = Vec2(a3.x - w, a3.y - h);
+//	//Vec2 poi[3] = { b1, b2, b3 };
+//	//setPhysics会重新设置锚点  --!!
+//	Vec2 poi[3] = { a1, a2, a3 };
+//	//PHYSICSBODY_MATERIAL_DEFAULT, Vec2::ZERO
+//	PhysicsBody*sanBody = PhysicsBody::createPolygon(poi, 3, PHYSICSBODY_MATERIAL_DEFAULT, Vec2::ZERO);
+//	sanBody->getShape(0)->setRestitution(0);
+//	sanBody->getShape(0)->setFriction(1.0f);
+//	//sanBody->setGravityEnable(false);
+//	sanBody->getShape(0)->setDensity(0.0f);
+//	sanBody->getShape(0)->setMass(200);
+//	/*auto san = Sprite::create();
+//	san->setPosition(a);
+//	san->setPhysicsBody(sanBody);*/
+//	//sanBody->setDynamic(false);
+//	//sanBody->getShape(0)->recenterPoints(poi, 3, Vec2(0,0));
+//	DrawNode* draw = DrawNode::create();
+//	//draw->setAnchorPoint(Vec2::ZERO);
+//	draw->drawTriangle(a1, a2, a3, Color4F(1, 1, 1, 1));
+//	draw->setPhysicsBody(sanBody);
+//	//sanBody->getShape(0)->recenterPoints(poi, 3, Vec2::ANCHOR_MIDDLE);
+//	return draw;
+//
+//}
+//Sprite*PhysicsWor::addsan(Vec2 a, std::vector<Vec2>*pos)
+//{
+//	Size visibleSize = Director::getInstance()->getVisibleSize();
+//	Point points[3];
+//	int j = 0;
+//	auto i = pos->begin();
+//	while (i != pos->end()){
+//		points[j++] = *i;
+//		i++;
+//	}
+//	Vec2 a1 = points[0];
+//	Vec2 a2 = points[1];
+//	Vec2 a3 = points[2];
+//	/*float w = (a3 + a1).x / 2;
+//	float h = (a2 + a1).y / 2;
+//	Vec2 b2 = Vec2(a2.x - w, a2.y - h);
+//	Vec2 b1 = Vec2(a1.x - w, a1.y - h);
+//	Vec2 b3 = Vec2(a3.x - w, a3.y - h);
+//	Vec2 poi[3] = { b1, b2, b3 };*/
+//	Vec2 poi[3] = { a1, a2, a3 };
+//	PhysicsBody*sanBody = PhysicsBody::createPolygon(poi, 3);
+//	auto san = Sprite::create();
+//	auto aa = a + Vec2(900, 200);
+//	san->setPosition(aa);
+//	san->setPhysicsBody(sanBody);
+//	/*	DrawNode* draw = DrawNode::create();
+//		draw->setAnchorPoint(Vec2::ZERO);
+//		draw->drawTriangle(a1,a2,a3,Color4F(1, 1, 1, 1));
+//		draw->setPhysicsBody(sanBody);*/
+//	return san;
+//}
 
 void PhysicsWor::Joint1(Vec2 p, Vec2 s, PhysicsWorld*world)
 {
@@ -106,14 +256,12 @@ void PhysicsWor::Joint1(Vec2 p, Vec2 s, PhysicsWorld*world)
 		-c->getContentSize().height / 2), 10, 5);
 	auto b = PhysicsWor::addBall(p + Vec2(1 * c->getContentSize().width / 3,
 		-c->getContentSize().height / 2), 10, 5);
-
 	addChild(a, 1);
 	addChild(b, 1);
 	addChild(c, 1);
 	PhysicsBody* aBody = a->getPhysicsBody();
 	PhysicsBody* bBody = b->getPhysicsBody();
 	PhysicsBody* cBody = c->getPhysicsBody();
-
 	PhysicsJointDistance *joint1 = PhysicsJointDistance::construct(cBody, aBody,
 		Vec2(0, a->getContentSize().height / 2), Vec2(0, 0));
 	PhysicsJointDistance *joint2 = PhysicsJointDistance::construct(cBody, bBody,
@@ -127,6 +275,30 @@ void PhysicsWor::Joint1(Vec2 p, Vec2 s, PhysicsWorld*world)
 	world->addJoint(joint3);
 	world->addJoint(joint4);
 }
+
+//void PhysicsWor::Joint3(Vec2 p, PhysicsWorld*world)
+//{
+//	Size visibleSize = Director::getInstance()->getVisibleSize();
+//	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+//	//auto sanA = Sprite::create("3.png");
+//	//sanA->setPosition(p);
+//	//Point points[3] = { Point(-29.00000, -25.00000), Point(-1.00000, 28.00000), Point(31.00000, -25.00000) };
+//	//auto sanABody = PhysicsBody::createPolygon(points, 3);
+//	//sanABody->setDynamic(false);
+//	//sanA->setPhysicsBody(sanABody);
+//	//addChild(sanA, 1);
+//
+//	//auto boxB = Sprite::create("1.png", Rect(p.x, p.y, 300, 20));
+//	//boxB->setPosition(p + Vec2(0, 44));
+//	//auto boxBBody = PhysicsBody::createBox(boxB->getContentSize());
+//
+//	//boxBBody->setGravityEnable(true);
+//	//boxB->setPhysicsBody(boxBBody);
+//	//addChild(boxB, 1);
+//
+//	//PhysicsJointDistance*joint = PhysicsJointDistance::construct(sanABody, boxBBody,
+//	//	Vec2(0, sanA->getContentSize().height / 2), Vec2(0, -boxB->getContentSize().height / 2));
+//	//world->addJoint(joint);
 
 void PhysicsWor::Joint3(Vec2 p, std::vector<Vec2>* pos, float w, float h, PhysicsWorld*world)
 {
